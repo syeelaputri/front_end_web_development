28dd74d09a2bf76e7a47cac5e854921b8e62cd4e
Syeela Putri <s22310495@student.unklab.ac.id>
2025-06-18 11:30:41 +0800

    exercise 6

---
+++ front-end/src/Card-Info.js
@@ -0,0 +1,16 @@
+import Evolution from "./Evolution.js";
+import TypeEffect from "./Type-Effect.js";
+import Logo from "./Logo.js";
+
+const CardInfo = () => {
+  return (
+    <>
+      <h1>Card-Info</h1>
+      <Evolution />
+      <TypeEffect />
+      <Logo />
+    </>
+  );
+};
+
+export default CardInfo;
---
+++ front-end/src/Type-Effect.js
@@ -0,0 +1,5 @@
+const TypeEffect = () => {
+  return <h1>Type-Effect</h1>;
+};
+
+export default TypeEffect;
